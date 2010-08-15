2390e53a686ceb856ff173c4fced72122b41c2cf
Mathias Ertl <mati@fsinf.at>
2010-08-15 20:00:33 +0000

    accept everything but a slash as usernames

--- Users/urls.py
+++ Users/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns( 'RestAuth.Users.views',
         (r'^$', 'create'),
-	(r'^(?P<username>\w+)/$', 'user_handler' ),
+	(r'^(?P<username>[^/]+)/$', 'user_handler' ),
 )
