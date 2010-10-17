ed159ffd359bd79f0e2fbe112065f922117732e6
Mathias Ertl <mati@fsinf.at>
2010-10-17 00:46:55 +0000

    new InternalAuthenticationBackend

---
+++ RestAuth/BasicAuth/backend.py
@@ -0,0 +1,26 @@
+import sys, base64
+from RestAuth.BasicAuth.models import Service
+
+class InternalAuthenticationBackend:
+	def authenticate( self, header, host ):
+		"""
+		Authenticate against a header as send by HTTP basic
+		authentication and a host. This method takes care of decoding
+		the header.
+		"""
+		method, data = header.split()
+		if method.lower() != 'basic':
+			return None # we only support basic authentication
+
+		name, password = base64.b64decode(data).split(':')
+
+		try:
+			serv = Service.objects.get( username=name )
+			if serv.verify( password, host ):
+				# service successfully verified
+				return serv
+		except Service.DoesNotExist:
+			# service does not exist
+			return None
+
+				
