3a92f07d1424aa567fc9ec888f896e891e41ac3c
Mathias Ertl <mati@fsinf.at>
2010-10-20 20:51:36 +0000

    only add comments

--- RestAuth/BasicAuth/decorator.py
+++ RestAuth/BasicAuth/decorator.py
@@ -17,25 +17,37 @@ from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
 
 def login_user( view, request, realm, *args, **kwargs ):
+	"""
+	The function called when the login_user decorator is used.
+	"""
 	if request.user.is_authenticated():
 		return view(request, *args, **kwargs)
 
 	if 'REMOTE_USER' in request.META:
+		# The web-server already authenticated the remote user:
 		user = authenticate( remote_user=request.META['REMOTE_USER'] )
 	if 'HTTP_AUTHORIZATION' in request.META:
+		# The web-server does *not* handle authentication and the client
+		# tries to authenticate.
 		header = request.META['HTTP_AUTHORIZATION']
 		host = request.META['REMOTE_ADDR']
 		user = authenticate( header=header, host=host )
 
 	if user:
+		# log the user in:
 		login(request, user)
 		return view(request, *args, **kwargs)
 	else:
+		# send an authentication challenge:
 		response = HttpResponse( "Please authenticate", status=401 )
 		response['WWW-Authenticate'] = 'Basic realm="%s"' % realm
 		return response
 
 def login_required( function = None, realm = "" ):
+	"""
+	Define our own login_required decorator to handle basic authentication,
+	performed either by the webserver or the decorator itself.
+	"""
 	def view_decorator(func):
 		def wrapper(request, *args, **kwargs):
 			return login_user(func, request, realm, *args, **kwargs)
