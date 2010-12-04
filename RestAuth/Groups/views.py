97186650ac7edc56b04790ce5a2e72b161ddad58
Mathias Ertl <mati@fsinf.at>
2010-12-04 12:40:28 +0000

    adapt /groups/ interface. minor optimizations

--- RestAuth/Groups/views.py
+++ RestAuth/Groups/views.py
@@ -16,129 +16,144 @@
 from RestAuth.Services.decorator import login_required
 from RestAuth.Users.models import *
 from RestAuth.Groups.models import *
-from RestAuth.common import marshal, parse_request_body
-from django.http import HttpResponse, QueryDict
-
-import sys
+from RestAuth.common import marshal
+from RestAuth.common.types import get_dict
+from django.http import HttpResponse
 
 @login_required( realm='/groups/' )
 def index( request ):
 	service = request.user
 
-	if request.method == 'GET':
-		if 'user' in request.GET:
-			# get groups for a specific user
-			
-			# If User.DoesNotExist: 404 Not Found
-			user = user_get( request.GET['user'] ) 
-			if 'nonrecursive' in request.GET:
-				recursive = False
-			else:
-				recursive = True
-
-			groups = user.get_groups( service, recursive )
-		else:
-			# get all groups for this service
-			groups = Group.objects.filter( service=service )
+	if request.method == 'GET': # Get a list of groups:
+		# get all groups for this service
+		groups = Group.objects.filter( service=service )
 
 		names = [ group.name for group in groups ]
 		# If MarshalError: 500 Internal Server Error
 		response = marshal( request, names )
 		return HttpResponse( response, status=200 ) # Ok
-	elif request.method == 'POST':
-		body = parse_request_body( request )
+	elif request.method == 'GET' and 'user' in request.GET: 
+		# Get all users in a group
+			
+		# If User.DoesNotExist: 404 Not Found
+		user = user_get( request.GET['user'] ) 
 
-		# add a new group
-		if 'group' in body:
-			groupname = body['group']
-		else:
-			return HttpResponse( status=400 ) # Bad request
+		groups = user.get_groups( service )
+		names = [ group.name for group in groups ]
+
+		response = marshal( request, names )
+		return HttpResponse( response, status=200 ) # Ok
+	elif request.method == 'POST': # Create a group
+		# If BadRequest: 400 Bad Request
+		groupname = get_dict( request, [ u'group' ] )
 
 		# If ResourceExists: 409 Conflict
 		group_create( groupname, service )
+# TODO: Location header
 		return HttpResponse( status=201 ) # Created
-	else:
-		return HttpResponse( status=405 ) # method not allowed
+	
+	return HttpResponse( status=405 ) # method not allowed
 
 @login_required( realm='/groups/<group>/' )
 def group_handler( request, groupname ):
 	service = request.user
 	
-	if request.method == 'GET':
-		# If Group.DoesNotExist: 404 Not Found
-		group = group_get( groupname, service )
+	# If Group.DoesNotExist: 404 Not Found
+	group = group_get( groupname, service )
+	
+	if request.method == 'GET': # Verify that a group exists:
+		return HttpResponse( status=204 )
+	if request.method == 'DELETE': # Delete group
+		group.delete()
+		return HttpResponse( status=204 ) # OK
+	
+	return HttpResponse( status=405 )
 
-		# get all members of a group
-		if 'nonrecursive' in request.GET:
-			recursive = False
-		else:
-			recursive = True
-		
-		users = group.get_members( recursive )
+@login_required( realm='/groups/<group>/users/' )
+def group_users_index_handler( request, groupname ):
+	service = request.user
+	
+	# If Group.DoesNotExist: 404 Not Found
+	group = group_get( groupname, service )
+
+	if request.method == 'GET': # Get all users in a group
+		users = group.get_members()
 
 		# If MarshalError: 500 Internal Server Error
 		response = marshal( request, [ user.username for user in users ] )
 		return HttpResponse( response, status=200 )
-	elif request.method == 'POST':
-		body = parse_request_body( request )
-
-		# we get the user/group we want to add right away so that we
-		# throw a 404 *before* the parent-group is created:
-		if 'user' in body:
-			# If User.DoesNotExist: 404 Not Found
-			user = user_get( body['user'] )
-		elif 'group' in body:
-			# If Group.DoesNotExist: 404 Not Found
-			childgroup = group_get( body['group'], service )
-		else:
-			return HttpResponse( status=400 )
-
-		if 'autocreate' in body:
-			try:
-				group = group_create( groupname, service )
-			except ResourceExists:
-				# If Group.DoesNotExist: 404 Not Found
-				group = group_get( groupname, service )
-		else:
-			# If Group.DoesNotExist: 404 Not Found
-			group = group_get( groupname, service )
-
-		if 'user' in body: # add a user to a group
-			group.users.add( user )
-		if 'group' in body: # add a group to a group
-			group.groups.add( childgroup )
+	elif request.method == 'POST': # Add a user to a group
+		# If BadRequest: 400 Bad Request
+		username = get_dict( request, [ u'user' ] )
 		
+		# If User.DoesNotExist: 404 Not Found
+		user = user_get( username )
+		
+		group.users.add( user )
 		group.save()
-		return HttpResponse( status=200 )
-	elif request.method == 'DELETE':
-		# If Group.DoesNotExist: 404 Not Found
-		group = group_get( groupname, service )
-		group.delete()
-		return HttpResponse( status=200 ) # OK
-	else:
-		return HttpResponse( status=405 )
+		return HttpResponse( status=204 )
+	
+	return HttpResponse( status=405 )
 
-@login_required( realm='/groups/<group>/<user>/' )
-def member_handler( request, groupname, username ):
+@login_required( realm='/groups/<group>/users/<user>/' )
+def group_user_handler( request, groupname, username ):
 	service = request.user
 	
 	# If Group.DoesNotExist: 404 Not Found
 	group = group_get( groupname, service )
 	# If User.DoesNotExist: 404 Not Found
 	user = user_get( username )
-
-	if request.method == 'GET':
-		if 'nonrecursive' in request.GET:
-			recursive = False
+	
+	if request.method == 'GET': # Verify that a user is in a group
+		if group.is_member( user ):
+			return HttpResponse( status=204 )
 		else:
-			recursive = True
-
-		if group.is_member( user, recursive ):
-			return HttpResponse( status=200 )
-		else:
+			# 404 Not Found
 			raise ServiceUser.DoesNotExist()
-	elif request.method == 'DELETE':
+	elif request.method == 'DELETE': # Remove user from a group
 		group.users.remove( user )
-		return HttpResponse( status=200 )
-	else:
-		return HttpResponse( status=405 )
+# TODO: 404 if the user was not in the group
+		return HttpResponse( status=204 )
+
+	return HttpResponse( status=405 )
+
+@login_required( realm='/groups/<group>/groups/' )
+def group_groups_index_handler( request, groupname ):
+	service = request.user
+	
+	# If Group.DoesNotExist: 404 Not Found
+	group = group_get( groupname, service )
+	if request.method == 'GET': # get a list of sub-groups
+		groups = group.groups.filter( service=service )
+
+		# If MarshalError: 500 Internal Server Error
+		body = marshal( request, [ group.name for group in groups ] )
+		return HttpResponse( body )
+	elif request.method == 'POST': # Add a sub-group:
+		# If BadRequest: 400 Bad Request
+		sub_groupname = get_dict( request, [ u'group' ] )
+		
+		# If Group.DoesNotExist: 404 Not Found
+		sub_group = group_get( sub_groupname, service )
+		
+		group.groups.add( sub_group )
+		group.save()
+		return HttpResponse( status=204 )
+
+	return HttpResponse( status=405 )
+
+@login_required( realm='/groups/<meta-group>/groups/<sub-group>/' )
+def group_groups_handler( request, meta_groupname, sub_groupname ):
+	service = request.user
+	
+	# If Group.DoesNotExist: 404 Not Found
+	meta_group = group_get( meta_groupname, service )
+	# If Group.DoesNotExist: 404 Not Found
+	sub_group = group_get( sub_groupname, service )
+
+	if request.method == 'DELETE': # Remove group from a group
+		meta_group.groups.remove( sub_group )
+# TODO: 404 if the user was not in the group
+		return HttpResponse( status=204 )
+	
+	return HttpResponse( status=405 )
