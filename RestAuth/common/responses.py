92fe60f11172ce71216bd36be9b9b246a1b5cce8
Mathias Ertl <mati@fsinf.at>
2010-12-09 15:40:24 +0000

    common HttpResponse class HttpRestAuthResponse and specialized classes for 201 Created and 204 No Content

--- RestAuth/common/responses.py
+++ RestAuth/common/responses.py
@@ -1,14 +1,22 @@
-from django.http import HttpResponse
-from RestAuth.common.types import serialize
+from django.http import HttpResponse as HttpResponseBase
+from RestAuth.common.types import serialize, get_response_type
 
-class HttpResponseCreated( HttpResponse ):
+class HttpRestAuthResponse( HttpResponseBase ):
+	def __init__( self, request, response_object, status=200 ):
+		# Best match of Content-Type header:
+		typ = get_response_type( request )
+		body = serialize( typ, response_object )
+
+		HttpResponseBase.__init__( self, body, typ, status, typ )
+
+class HttpResponseNoContent( HttpRestAuthResponse ):
+	def __init__( self ):
+		HttpResponseBase.__init__( self, status=204 )
+
+class HttpResponseCreated( HttpRestAuthResponse ):
 	def __init__( self, request, entity ):
-# TODO: make_list of body
 		location = entity.get_absolute_url()
 		uri = request.build_absolute_uri( location )
 
-		# RFC 2616 defines body as a list:
-		body = serialize( request, [ uri ] )
-
-		HttpResponse.__init__( self, body, status=201 )
+		HttpRestAuthResponse.__init__( self, request, [ uri ], 201 )
 		self['Location'] = uri
