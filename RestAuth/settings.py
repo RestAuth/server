7d31d422f8ac288936d9778ea739a2173eee342d
Mathias Ertl <mati@fsinf.at>
2011-01-02 16:04:25 +0000

    added configuration to sessions and caching. Also no longer set already-set default values in settings.py.

--- RestAuth/settings.py
+++ RestAuth/settings.py
@@ -33,7 +33,7 @@ from djangosettings import *
 # Set debugging to "True" (without quotes) to get backtraces via HTTP. When set
 # to False, backtraces will be sent to the adresses listed in the ADMINS
 # variable.
-DEBUG = True
+DEBUG = False
 
 # Adresses that will receive backtraces when DEBUG=False
 ADMINS = (
@@ -59,11 +59,11 @@ DATABASES = {
 # timezone as the operating system.
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
-TIME_ZONE = 'Europe/Vienna'
+#TIME_ZONE = 'Europe/Vienna'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
+#LANGUAGE_CODE = 'en-us'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
@@ -80,13 +80,40 @@ SECRET_KEY = ''
 # You can use this setting to disable some validators so you can support a wider
 # range of usernames. Valid values are 'xmpp', 'email', 'mediawiki', 'linux' and
 # 'windows'.
-SKIP_VALIDATORS = [ 'windows' ]
+#SKIP_VALIDATORS = [ 'windows' ]
 
 # Set to True if RestAuth should allow the username of a user to be changed:
-ALLOW_USERNAME_CHANGE = False
+#ALLOW_USERNAME_CHANGE = False
 
 # You can override the minimum username and password length:
-MIN_USERNAME_LENGTH = 3
+#MIN_USERNAME_LENGTH = 3
+
+########################
+### Session handling ###
+########################
+# HTTP sessions are disabled by default. You may want to enable it for libraries
+# that require it. Note that this costs considerable performance.
+#MIDDLEWARE_CLASSES.insert( 1,
+#	'django.contrib.auth.middleware.AuthenticationMiddleware' )
+#MIDDLEWARE_CLASSES.insert( 1,
+#	'django.contrib.sessions.middleware.SessionMiddleware' )
+
+###############
+### CACHING ###
+###############
+# Django can use memcached to considerably speed up some requests. Note that due
+# the Django caching implementation, the current performance improvement is not
+# that great.
+
+# If you want to use caching of any type, you must first uncomment these lines:
+#MIDDLEWARE_CLASSES.insert( 0, 'django.middleware.cache.UpdateCacheMiddleware' )
+#MIDDLEWARE_CLASSES.append( 'django.middleware.cache.FetchFromCacheMiddleware' )
+
+# These are the required settings used by Django. See its documentation for
+# detailed informations on these settings.
+#CACHE_BACKEND = 'memcached://127.0.0.1:11211/'
+#CACHE_MIDDLEWARE_SECONDS = 300
+#CACHE_MIDDLEWARE_KEY_PREFIX = ''
 
 #################
 ### PASSWORDS ###
@@ -95,7 +122,7 @@ MIN_USERNAME_LENGTH = 3
 # All settings in this section are optional, the defaults are usually fine.
 
 # Reconfigure the minimum password length. Only affects new passwords.
-MIN_PASSWORD_LENGTH = 6
+#MIN_PASSWORD_LENGTH = 6
 
 # Set a different hash algorithm for hashing passwords. This only affects newly
 # created passwords, so you can safely change this at any time, old hashes will
@@ -105,7 +132,7 @@ MIN_PASSWORD_LENGTH = 6
 # default and recommended. Additionally, RestAuth supports using hashes
 # compatible with other systems. Currectly 'mediawiki' creates hashes compatible
 # with a MediaWiki database.
-HASH_ALGORITHM = 'sha512'
+#HASH_ALGORITHM = 'sha512'
 
 ###############
 ### LOGGING ###
@@ -117,9 +144,9 @@ HASH_ALGORITHM = 'sha512'
 #	(i.e. when a client assumes that a user exists but in fact does not)
 # * INFO:     Also log successfully processed requests that change data.
 # * DEBUG:    Also log idempotent requests, i.e. if a user exists, etc.
-LOG_LEVEL = 'DEBUG'
+#LOG_LEVEL = 'DEBUG'
 
 # Define the target for logging. LOG_TARGET may either be 'stdout', 'stderr' or
 # or a path to a filename. If RestAuth is run with mod_wsgi, 'stdout' does not 
 # work and 'stderr' logs to the global apache log-file, not the VHOST log file.
-LOG_TARGET = 'stderr'
+#LOG_TARGET = 'stderr'
