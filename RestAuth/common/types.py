e0f68079397450af2c83b4f233d279721074eadd
Mathias Ertl <mati@fsinf.at>
2011-01-16 16:36:15 +0000

    throw correct exceptions for 406 and 415

--- RestAuth/common/types.py
+++ RestAuth/common/types.py
@@ -11,7 +11,7 @@ def get_request_type( request ):
 	if match:
 		return match
 	else:
-		raise NotAcceptable()
+		raise UnsupportedMediaType()
 
 def get_response_type( request ):
 	import mimeparse
@@ -23,7 +23,7 @@ def get_response_type( request ):
 	if match:
 		return match
 	else:
-		raise UnsupportedMediaType()
+		raise NotAcceptable()
 
 def get_dict( request, keys ):
 	"""
