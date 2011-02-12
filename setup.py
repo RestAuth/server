ae1d0b5168113c4112c8d85ce1781320d395c2ea
Mathias Ertl <mati@fsinf.at>
2011-02-12 22:03:48 +0100

    automatically generate man-pages, django-settings.py. remove patch and unpatch targets, adapt do git in get_version

--- setup.py
+++ setup.py
@@ -14,14 +14,42 @@
 #    along with RestAuthClient.py.  If not, see <http://www.gnu.org/licenses/>.
 
 from subprocess import Popen, PIPE
-from os import listdir
-from os.path import normpath, isdir, basename, exists, isfile
+from os.path import normpath, isdir, basename, exists, isfile, splitext
 from shutil import copytree, ignore_patterns
 from distutils.core import setup, Command
 from distutils.command.install_data import install_data as _install_data
 from distutils.command.build import build as _build
 from distutils.command.clean import clean as _clean
 from distutils.util import execute
+import os, time, glob
+
+def get_version():
+	version = '0.0'
+	if exists( '.version' ): # get from file
+		version = open( '.version' ).readlines()[0]
+	elif exists( '.svn' ): # get from svn
+		cmd = [ 'svn', 'info' ]
+		p = Popen( cmd, stdout=PIPE )
+		stdin, stderr = p.communicate()
+		lines = stdin.split( "\n" )
+		line = [ line for line in lines if line.startswith( 'Revision' ) ][0]
+		version = '0.0-' + line.split( ': ' )[1].strip()
+	elif exists( '.git' ): # get from git
+		date = time.strftime( '%Y.%m.%d' )
+		cmd = [ 'git', 'rev-parse', '--short', 'HEAD' ]
+		p = Popen( cmd, stdout=PIPE )
+		stdin, stderr = p.communicate()
+		version = '%s-%s-%s'%(version, date, stdin.strip() )
+	return version
+
+def process_file( in_path, out_path, dictionary ):
+	in_file = open( in_path )
+	out_file = open( out_path, 'w' )
+	for line in in_file.readlines():
+		for key in dictionary.keys():
+			if key in line:
+				line = line.replace( key, dictionary[key] )
+		out_file.write( line )
 
 class install_data( _install_data ):
 	"""
@@ -45,142 +73,19 @@ class install_data( _install_data ):
 		_install_data.run( self )
 
 added_options = [('prefix=', None, 'installation prefix'),
-	('exec-prefix=', None, 'prefix for platform-specific files'),
-	('patch-dir=', None, 'Directory where patches are located.') ]
-
-class patch_command( Command ):
-	user_options = added_options
-	
-	def initialize_options( self ):
-		self.prefix = '/usr/local'
-		self.exec_prefix = '/usr/local/bin'
-		self.patch_dir = 'patches'
-
-	def finalize_options( self ):
-		command = self.get_command_name()
-		options = self.distribution.command_options[ command ]
-		
-		if 'prefix' in options:
-			self.prefix = options['prefix'][1]
-		if 'exec_prefix' in options:
-			self.exec_prefix = options['exec_prefix'][1]
-		if 'patch_dir' in options:
-			self.patch_dir = options['patch_dir'][1]
-	
-	def run( self ):
-		for patch in listdir( self.patch_dir ):
-			full_path = normpath( self.patch_dir + '/' + patch )
-
-			if isdir( full_path ):
-				continue
-
-			print( " Applying %s..."%(patch) )
-
-			f = open( full_path )
-			lines = f.readlines()
-			patch_lines = []
-			for line in lines:
-				line = line.replace( '__PREFIX__', self.prefix )
-				patch_lines.append( line )
-
-			patch = "".join( patch_lines )
-
-			# todo: already applied patches
-			cmd = [ 'patch' ] + self.__class__.patch_args
-			print( ' '.join( cmd ) )
-			p = Popen( cmd, stdin=PIPE )
-			p.stdin.write( patch )
-
-class patch( patch_command ):
-	description = "Patch source code before installation" 
-	patch_args = [ '-t', '-p0' ]
-	
-class unpatch( patch_command ):
-	description = "Patch source code before installation"
-	patch_args = [ '-R', '-t', '-p0' ]
-
-class build( _build ):
-	def has_patches(self):
-		if not self.patch_dir or not exists( self.patch_dir ):
-			return False
-
-		ls = listdir( self.patch_dir )
-		files = [ f for f in ls if isfile( "%s/%s"%(self.patch_dir, f) ) ]
-		if files:
-			return True
-
-		return False
-
-	def initialize_options( self ):
-		"""
-		Modify this class so that we also understand --install-dir.
-		"""
-		_build.initialize_options( self )
-		self.prefix = None
-		self.exec_prefix = None
-		self.patch_dir = None
-
-	sub_commands = [('patch', has_patches)] + _build.sub_commands
-	user_options = _build.user_options + added_options
-
-	def run( self ):
-		_build.run( self )
-
-		import string, random
-		chars = string.letters + string.digits + string.punctuation
-		SECRET_KEY = "".join( [random.choice(chars) for i in xrange(30)] )
-		SECRET_KEY = SECRET_KEY.replace( '\\', '\\\\' )
-		SECRET_KEY = SECRET_KEY.replace( '\'', '\\\\\'' )
-		SECRET_KEY = SECRET_KEY.replace( '/', '\/' )
-		SECRET_KEY = SECRET_KEY.replace( '&', '\&' )
-		sed_string = '''s/^SECRET_KEY\s*=\s*.*$/SECRET_KEY = \'\'\'%s\'\'\'/'''%SECRET_KEY
-
-		cmd = [ 'sed', '-i', sed_string, 'RestAuth/djangosettings.py' ]
-		print( ' '.join( cmd ) )
-		p = Popen( cmd )
-		p.communicate()
+	('exec-prefix=', None, 'prefix for platform-specific files') ]
 
 class clean( _clean ):
-	def has_patches( self ):
-		if not self.patch_dir or not exists( self.patch_dir ):
-			return False
-
-		ls = listdir( self.patch_dir )
-		files = [ f for f in ls if isfile( "%s/%s"%(self.patch_dir, f) ) ]
-		if files:
-			return True
-
-		return False
-
 	def initialize_options( self ):
 		_clean.initialize_options( self )
-		self.patch_dir = None
 		
-	sub_commands = [('unpatch', has_patches)] + _build.sub_commands
-	user_options = _clean.user_options + [ ('patch-dir=', None, 'Directory where patches are located.') ]
-
 	def run( self ):
 		_clean.run( self )
-		sed_string = '''s/^SECRET_KEY\s*=\s*.*$/SECRET_KEY = ''/'''
 
-		cmd = [ 'sed', '-i', sed_string, 'RestAuth/djangosettings.py' ]
-		print( ' '.join( cmd ) )
-		p = Popen( cmd )
-		p.communicate()
-
-def get_version():
-	version = '0.1'
-	if exists( '.version' ):
-		print( 'get version from file...' )
-		version = open( '.version' ).readlines()[0]
-	elif exists( '.svn' ):
-		cmd = [ 'svn', 'info' ]
-		p = Popen( cmd, stdout=PIPE )
-		stdin, stderr = p.communicate()
-		lines = stdin.split( "\n" )
-		line = [ line for line in lines if line.startswith( 'Revision' ) ][0]
-		version = '0.0-' + line.split( ': ' )[1].strip()
-	return version
+		# remove generated files
+		os.remove( 'RestAuth/djangosettings.py' )
+		for filename in glob.glob( 'man/*.[0-9]' ):
+			os.remove( filename )
 
 class version( Command ):
 	user_options = []
@@ -190,6 +95,113 @@ class version( Command ):
 		pass
 	def run( self ):
 		print( get_version() )
+
+class build_man( Command ):
+	user_options = []
+	description = "Build man pages"
+	def initialize_options( self ):
+		pass
+	def finalize_options( self ):
+		pass
+
+	def run( self ):
+		dictionary = { 
+			'__SETTINGS_OPTION__': '''\\fB\-\-settings\\fR=\\fISETTINGS\\fR
+.RS 4
+The path to the Django settings module. This is equivalent to setting the
+environment variable \\fBDJANGO_SETTINGS_MODULE\\fR. \\fISETTINGS\\fR should be the
+path to your \\fBsettings.py\\fR. The setting should be in Python path syntax, e.g.
+\\fBRestAuth.settings\\fR. The default is usually fine.
+.sp
+For additinal help, you might want to check out:
+.RS 4
+\\fIhttp://docs.djangoproject.com/en/dev/topics/settings/\\fR
+.sp
+.RE
+.RE
+''',
+			'__END__': '''.SH ENVIRONMENT
+\\fBrestauth-service\\fR uses the Django framework and as such is affected by its
+environment variables.
+.sp
+\\fBDJANGO_SETTINGS_MODULE\\fR
+.RS 4
+Setting this environment variable is equivalent to using the \\fB--settings\\fR
+option.
+.SH AUTHORS
+Copyright \(co 2010\-2011 by RestAuth engineers:
+.sp
+.RS 4
+.ie n \{\h'-04'\(bu\h'+03'\c
+.\}
+.el \{.sp -1
+.IP \(bu 2.3
+.\}
+Mathias Ertl <mati@fsinf\&.at>
+.RE
+." template end ;)
+.sp
+License GPLv3+: GNU GPL version 3 or later.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+.sp
+See http://gnu.org/licenses/gpl.html for more information.
+.SH SEE ALSO
+\\fBrestauth-user\\fP(8), \\fBrestauth-group\\fP(8)
+.sp
+For more information, see the RestAuth homepage at
+.RS 4
+\\fIhttps://redmine.fsinf.at/projects/restauth-server\\fR
+.RE
+.SH BUGS
+Please submitt bugs to our issue tracker:
+.RS 4
+\\fIhttps://redmine.fsinf.at/projects/restauth-server/issues\\fR
+'''}
+
+		version = get_version()
+		date = time.strftime( '%Y-%m-%d' )
+		for filename in glob.glob( 'man/*.in' ):
+			output_path, ext = splitext( filename )
+			man_name, ext = splitext( basename( output_path ) )
+			dictionary['__HEADER__'] = '''."
+."     Title: %s
+."    Author: Mathias Ertl <mati@fsinf.at>
+."  Language: English
+."      Date: %s
+."
+.TH %s 8  "%s" "Version %s" "RestAuth Manual"
+'''%(man_name, date, man_name, date, version)
+			process_file( filename, output_path, dictionary )
+
+class build( _build ):
+	def initialize_options( self ):
+		"""
+		Modify this class so that we also understand --install-dir.
+		"""
+		_build.initialize_options( self )
+		self.prefix = None
+		self.exec_prefix = None
+
+	sub_commands = [('build_man', lambda self: True)] + _build.sub_commands
+	user_options = _build.user_options + added_options
+
+	def run( self ):
+		# generate secret key:
+		import string, random
+		chars = string.letters + string.digits + string.punctuation
+		SECRET_KEY = "".join( [random.choice(chars) for i in xrange(30)] )
+		SECRET_KEY = SECRET_KEY.replace( '\\', '\\\\' )
+		SECRET_KEY = SECRET_KEY.replace( '\'', '\\\\\'' )
+		SECRET_KEY = SECRET_KEY.replace( '/', '\/' )
+		SECRET_KEY = SECRET_KEY.replace( '&', '\&' )
+
+		in_file = 'RestAuth/djangosettings.py.in'
+		out_file = 'RestAuth/djangosettings.py'
+		dictionary = { '__SECRET_KEY__':SECRET_KEY }
+		process_file( in_file, out_file, dictionary )
+		
+		_build.run( self )
 
 setup(
 	name='RestAuth',
@@ -206,5 +218,5 @@ setup(
 		],
 	cmdclass={
 		'install_data': install_data, 'build': build, 'version': version,
-		'clean': clean, 'patch': patch, 'unpatch': unpatch },
+		'clean': clean, 'build_man': build_man },
 )
