46cec3f3325db70fe852a779ce65d6ad73200566
Mathias Ertl <mati@fsinf.at>
2011-03-23 16:44:50 +0100

    default to IPv6

--- test.sh
+++ test.sh
@@ -19,5 +19,5 @@ export PYTHONPATH="$PWD"
 
 rm -f ./RestAuth.sqlite3
 python RestAuth/manage.py syncdb --noinput
-bin/restauth-service.py --password=vowi add vowi 127.0.0.1
-python RestAuth/manage.py runserver
+bin/restauth-service.py --password=vowi add vowi ::1
+python RestAuth/manage.py runserver --ipv6
