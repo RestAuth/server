833de701a0121a7031f609d9a6fe264d51600a2b
Mathias Ertl <mati@fsinf.at>
2011-07-23 01:51:46 +0200

    cleanup imports

--- bin/restauth-service.py
+++ bin/restauth-service.py
@@ -15,8 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
-import os, sys, getpass, random, string
-from argparse import ArgumentParser, Action
+import os, sys, getpass
 
 # Setup environment
 if 'DJANGO_SETTINGS_MODULE' not in os.environ:
