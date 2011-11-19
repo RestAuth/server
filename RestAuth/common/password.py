c180cb2ea7b3588f001bd5c02356179ab54eb858
Mathias Ertl <mati@fsinf.at>
2011-11-19 15:39:14 +0100

    support crypt and apr1 encryption algorithms

--- RestAuth/common/password.py
+++ RestAuth/common/password.py
@@ -16,7 +16,71 @@
 # along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.utils.encoding import smart_str
-import hashlib
+import hashlib, crypt
+
+def crypt_apr1_md5( plainpasswd, salt ):
+	import struct, base64, string
+	def pack( val ):
+		md5 = hashlib.md5( val ).hexdigest()
+		return struct.pack('16B', *[int(c, 16) for c in (md5[i:i+2] for i in xrange(0, len(md5), 2))])
+	
+	length = len( plainpasswd )
+	text = "%s$apr1$%s"%(plainpasswd, salt)
+	md5 = hashlib.md5( "%s%s%s"%(plainpasswd, salt, plainpasswd) ).hexdigest()
+	bin = pack( "%s%s%s"%(plainpasswd, salt, plainpasswd) )
+	
+	# first loop
+	i = len( plainpasswd )
+	while i > 0:
+		text += bin[0:min(16,i)]
+		i -= 16
+		
+	# second loop
+	i = len( plainpasswd )
+	while i > 0:
+		if i & 1:
+			text += chr(0)
+		else:
+			text += plainpasswd[0]
+			
+		# shift bits by one (/2 rounded down)
+		i >>= 1
+	
+	# 1000er loop
+	bin = pack( text )
+	for i in range(0, 1000):
+		if i & 1:
+			new = plainpasswd
+		else:
+			new = bin
+
+		if i % 3:
+			new += salt
+		if i % 7:
+			new += plainpasswd
+		
+		if i & 1:
+			new += bin
+		else:
+			new += plainpasswd
+
+		bin = pack( new )
+
+	tmp = ''
+	for i in range(0, 5):
+		k = i+6
+		j = i+12
+		if j == 16:
+			j = 5
+		
+		tmp = "%s%s%s%s"%(bin[i], bin[k], bin[j], tmp)
+
+	tmp = "%s%s%s%s"%(chr(0), chr(0), bin[11], tmp)
+	
+	frm = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+	to = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+	trans = string.maketrans( frm, to )
+	return base64.b64encode( tmp )[2:][::-1].translate( trans )
 
 def get_salt():
 	"""
@@ -35,9 +99,20 @@ def get_hexdigest( algorithm, salt, secret ):
 
 	Unlike the django function, this function requires python2.5 or higher.
 	"""
-	salt, secret = smart_str(salt), smart_str(secret)
+	secret = smart_str(secret)
+	if salt:
+		salt = smart_str(salt)
+		
 	if algorithm == 'mediawiki': # pragma: no cover
 		secret_hash = hashlib.md5( secret ).hexdigest()
 		return hashlib.md5( '%s-%s'%(salt, secret_hash ) ).hexdigest()
+	elif algorithm == 'crypt':
+		return crypt.crypt( secret, salt )[2:]
+	elif algorithm == 'apr1':
+		return crypt_apr1_md5( secret, salt ).encode( 'utf-8' )
 	else:
-		return getattr( hashlib, algorithm )( salt + secret ).hexdigest()
+		func = getattr( hashlib, algorithm )
+		if salt:
+			return func( salt + secret ).hexdigest()
+		else:
+			return func( secret ).hexdigest()
