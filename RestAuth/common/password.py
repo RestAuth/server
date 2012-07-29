50b442481291c68c78166569b981da349ed1ac89
Mathias Ertl <mati@fsinf.at>
2012-07-29 22:18:11 +0200

    make file pep8 clean

--- RestAuth/common/password.py
+++ RestAuth/common/password.py
@@ -15,33 +15,39 @@
 # You should have received a copy of the GNU General Public License
 # along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
+import base64
+import crypt
+import hashlib
+import struct
+import string
+
 from django.utils.encoding import smart_str
-import hashlib, crypt
+
 
 def crypt_apr1_md5(plainpasswd, salt):
     """
-    This function creates an md5 hash that is identical to one that would be created by
-    :py:cmd:`htpasswd -m`.
-    
+    This function creates an md5 hash that is identical to one that would be
+    created by :cmd:`htpasswd -m`.
+
     Algorithm shamelessly copied from here:
         http://www.php.net/manual/de/function.crypt.php#73619
     """
-    import struct, base64, string
     def pack(val):
         md5 = hashlib.md5(val).hexdigest()
-        return struct.pack('16B', *[int(c, 16) for c in (md5[i:i+2] for i in xrange(0, len(md5), 2))])
-    
+        cs = [md5[i:i + 2] for i in xrange(0, len(md5), 2)]
+        return struct.pack('16B', *[int(c, 16) for c in cs])
+
     length = len(plainpasswd)
     text = "%s$apr1$%s" % (plainpasswd, salt)
     md5 = hashlib.md5("%s%s%s" % (plainpasswd, salt, plainpasswd)).hexdigest()
     bin = pack("%s%s%s" % (plainpasswd, salt, plainpasswd))
-    
+
     # first loop
     i = len(plainpasswd)
     while i > 0:
-        text += bin[0:min(16,i)]
+        text += bin[0:min(16, i)]
         i -= 16
-        
+
     # second loop
     i = len(plainpasswd)
     while i > 0:
@@ -49,10 +55,10 @@ def crypt_apr1_md5(plainpasswd, salt):
             text += chr(0)
         else:
             text += plainpasswd[0]
-            
+
         # shift bits by one (/2 rounded down)
         i >>= 1
-    
+
     # 1000er loop
     bin = pack(text)
     for i in range(0, 1000):
@@ -65,7 +71,7 @@ def crypt_apr1_md5(plainpasswd, salt):
             new += salt
         if i % 7:
             new += plainpasswd
-        
+
         if i & 1:
             new += bin
         else:
@@ -75,19 +81,20 @@ def crypt_apr1_md5(plainpasswd, salt):
 
     tmp = ''
     for i in range(0, 5):
-        k = i+6
-        j = i+12
+        k = i + 6
+        j = i + 12
         if j == 16:
             j = 5
-        
+
         tmp = "%s%s%s%s" % (bin[i], bin[k], bin[j], tmp)
 
     tmp = "%s%s%s%s" % (chr(0), chr(0), bin[11], tmp)
-    
+
     frm = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
     to = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     trans = string.maketrans(frm, to)
     return base64.b64encode(tmp)[2:][::-1].translate(trans)
+
 
 def get_salt():
     """
@@ -97,6 +104,7 @@ def get_salt():
     from random import random
     random_string = ','.join(map(lambda a: str(random()), range(5)))
     return hashlib.sha512(random_string).hexdigest()[:8]
+
 
 def get_hexdigest(algorithm, salt, secret):
     """
@@ -109,8 +117,8 @@ def get_hexdigest(algorithm, salt, secret):
     secret = smart_str(secret)
     if salt:
         salt = smart_str(salt)
-        
-    if algorithm == 'mediawiki': # pragma: no cover
+
+    if algorithm == 'mediawiki':  # pragma: no cover
         secret_hash = hashlib.md5(secret).hexdigest()
         return hashlib.md5('%s-%s' % (salt, secret_hash)).hexdigest()
     elif algorithm == 'crypt':
