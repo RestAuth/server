809931174d98cdc58d3e06f44e5600554a50c628
Mathias Ertl <mati@fsinf.at>
2012-07-29 22:10:16 +0200

    make file pep8 clean

--- RestAuth/common/responses.py
+++ RestAuth/common/responses.py
@@ -19,6 +19,7 @@ from django.http import HttpResponse as HttpResponseBase
 from RestAuth.common.types import get_response_type
 from RestAuthCommon.handlers import CONTENT_HANDLERS
 
+
 class HttpRestAuthResponse(HttpResponseBase):
     def __init__(self, request, response_object, status=200):
         mime_type = get_response_type(request)
@@ -27,14 +28,16 @@ class HttpRestAuthResponse(HttpResponseBase):
 
         HttpResponseBase.__init__(self, body, mime_type, status, mime_type)
 
+
 class HttpResponseNoContent(HttpRestAuthResponse):
     def __init__(self):
         HttpResponseBase.__init__(self, status=204)
+
 
 class HttpResponseCreated(HttpRestAuthResponse):
     def __init__(self, request, entity):
         location = entity.get_absolute_url()
         uri = request.build_absolute_uri(location)
 
-        HttpRestAuthResponse.__init__(self, request, [ uri ], 201)
+        HttpRestAuthResponse.__init__(self, request, [uri], 201)
         self['Location'] = uri
