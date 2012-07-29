c54a3f07c678b2c8902d168db047d93ddfb54474
Mathias Ertl <mati@fsinf.at>
2012-07-29 23:39:59 +0200

    make this file pep8 clean

--- RestAuth/Groups/urls.py
+++ RestAuth/Groups/urls.py
@@ -15,13 +15,16 @@
 # You should have received a copy of the GNU General Public License
 # along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns
 
-urlpatterns = patterns('RestAuth.Groups.views',
+urlpatterns = patterns(
+    'RestAuth.Groups.views',
     (r'^$', 'index'),
-    (r'^(?P<groupname>[^/]+)/$', 'group_handler' ),
-    (r'^(?P<groupname>[^/]+)/users/$', 'group_users_index_handler' ),
-    (r'^(?P<groupname>[^/]+)/users/(?P<username>[^/]+)/$', 'group_user_handler' ),
-    (r'^(?P<groupname>[^/]+)/groups/$', 'group_groups_index_handler' ),
-    (r'^(?P<meta_groupname>[^/]+)/groups/(?P<sub_groupname>[^/]+)/$', 'group_groups_handler' ),
+    (r'^(?P<groupname>[^/]+)/$', 'group_handler'),
+    (r'^(?P<groupname>[^/]+)/users/$', 'group_users_index_handler'),
+    (r'^(?P<groupname>[^/]+)/users/(?P<username>[^/]+)/$',
+     'group_user_handler'),
+    (r'^(?P<groupname>[^/]+)/groups/$', 'group_groups_index_handler'),
+    (r'^(?P<meta_groupname>[^/]+)/groups/(?P<sub_groupname>[^/]+)/$',
+     'group_groups_handler'),
 )
