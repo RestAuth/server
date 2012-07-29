ed1f0044521a58f3656745e6d57827c8aa0f582b
Mathias Ertl <mati@fsinf.at>
2012-07-30 00:47:11 +0200

    make urls.py pep8 clan

--- RestAuth/urls.py
+++ RestAuth/urls.py
@@ -15,6 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
+from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
 
 urlpatterns = patterns(
