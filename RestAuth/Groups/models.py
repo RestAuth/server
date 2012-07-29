00394dc6cab03498a1139dc102e069d46bc013cd
Mathias Ertl <mati@fsinf.at>
2012-07-30 00:06:27 +0200

    make this file pep8 clean

--- RestAuth/Groups/models.py
+++ RestAuth/Groups/models.py
@@ -15,16 +15,16 @@
 # You should have received a copy of the GNU General Public License
 # along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
+from django.contrib.auth.models import User as Service
 from django.db import models
 from django.db.utils import IntegrityError
-
-from django.contrib.auth.models import User as Service
 from django.utils.translation import ugettext_lazy as _
 from django.utils.http import urlquote
 
+from RestAuthCommon.error import PreconditionFailed
+
 from RestAuth.Users.models import ServiceUser as User
 from RestAuth.common.errors import GroupExists
-from RestAuthCommon.error import PreconditionFailed
 
 group_permissions = (
     ('groups_for_user', 'List groups for a user'),
@@ -41,6 +41,7 @@ group_permissions = (
     ('group_remove_group', 'Remove a subgroup from a group'),
 )
 
+
 def group_create(name, service=None):
     """
     Create a new group.
@@ -56,32 +57,38 @@ def group_create(name, service=None):
     except IntegrityError:
         raise GroupExists('Group "%s" already exists' % name)
 
-# Create your models here.
+
 class Group(models.Model):
-    service = models.ForeignKey(Service, null=True,
-        help_text=_("Service that is associated with this group."))
-    name = models.CharField(_('name'), max_length=30, db_index=True, 
-        help_text=_("Required. Name of the group."))
+    service = models.ForeignKey(
+        Service, null=True,
+        help_text=_("Service that is associated with this group.")
+    )
+    name = models.CharField(
+        _('name'), max_length=30, db_index=True,
+        help_text=_("Required. Name of the group.")
+    )
     users = models.ManyToManyField(User)
-    groups = models.ManyToManyField('self', symmetrical=False, related_name='parent_groups')
-    
+    groups = models.ManyToManyField(
+        'self', symmetrical=False, related_name='parent_groups')
+
     class Meta:
         unique_together = ('name', 'service')
         permissions = group_permissions
-    
+
     def __init__(self, *args, **kwargs):
         models.Model.__init__(self, *args, **kwargs)
         from RestAuthCommon import resource_validator
 
         if self.name and not resource_validator(self.name):
-            raise PreconditionFailed("Name of group contains invalid characters")
+            raise PreconditionFailed(
+                "Name of group contains invalid characters")
 
     def get_members(self, recursive=True, lvl=0):
         users = set(self.users.values_list('username', flat=True))
-        
+
         if recursive and lvl < 10:
             for parent in self.parent_groups.only('name'):
-                users = users.union(parent.get_members(recursive, lvl+1))
+                users = users.union(parent.get_members(recursive, lvl + 1))
 
         return users
 
@@ -91,7 +98,7 @@ class Group(models.Model):
 
         if recursive and lvl < 10:
             for parent in self.parent_groups.only('name'):
-                if parent.is_member(user, recursive, lvl+1):
+                if parent.is_member(user, recursive, lvl + 1):
                     return True
         return False
 
@@ -100,21 +107,21 @@ class Group(models.Model):
             if parent.is_member(user, True):
                 return True
         return False
-    
+
     def save(self, *args, **kwargs):
-        if self.service == None:
+        if self.service is not None:
             conflict = Group.objects.filter(name=self.name, service=None)
             if self.id:
                 conflict.exclude(pk=self.id)
             if conflict.exists():
                 raise IntegrityError("columns name, service_id are not unique")
         super(Group, self).save(*args, **kwargs)
-    
-    def __unicode__(self): # pragma: no cover
+
+    def __unicode__(self):  # pragma: no cover
         if self.service:
-            return "%s/%s"%(self.name, self.service.username)
+            return "%s/%s" % (self.name, self.service.username)
         else:
-            return "%s/None"%(self.name)
-            
+            return "%s/None" % (self.name)
+
     def get_absolute_url(self):
-        return '/groups/%s/'%  urlquote(self.name)
+        return '/groups/%s/' % urlquote(self.name)
