146d8896ef1ee027cbc45f6514388810d842c4e7
Mathias Ertl <mati@fsinf.at>
2012-08-17 23:49:26 +0200

    final state for model

--- RestAuth/Services/models.py
+++ RestAuth/Services/models.py
@@ -48,10 +48,6 @@ def service_create(name, password, *hosts):
 
 
 class Service(User):
-    hosts = models.ManyToManyField('ServiceAddress')
-#    class Meta:
-#        proxy = True
-
     def verify(self, password, host):
         if self.check_password(password) and self.verify_host(host):
             return True
@@ -97,7 +93,7 @@ class Service(User):
 
 class ServiceAddress(models.Model):
     address = models.CharField(max_length=39, unique=True)
-    services = models.ManyToManyField(Service, related_name='hosts_new')
+    services = models.ManyToManyField(Service, related_name='hosts')
 
     def __unicode__(self):  # pragma: no cover
         return self.address
