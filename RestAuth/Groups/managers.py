8c3b2cd50b943809b2312cc95fd91842294d795e
Mathias Ertl <mati@fsinf.at>
2012-08-26 23:05:09 +0200

    add GroupManager and GroupQuerySet with the new member() function

---
+++ RestAuth/Groups/managers.py
@@ -0,0 +1,9 @@
+from django.db import models
+from RestAuth.Groups.querysets import GroupQuerySet
+
+class GroupManager(models.Manager):
+    def get_query_set(self):
+        return GroupQuerySet(self.model)
+
+    def member(self, user, service=None, depth=None):
+        return self.get_query_set().member(user, service, depth)
