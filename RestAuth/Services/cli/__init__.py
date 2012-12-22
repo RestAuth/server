60e2dc95c95b21af256380c636ba13cd5d490992
Mathias Ertl <mati@fsinf.at>
2012-12-22 21:01:29 +0100

    move PermissionParser to Services.cli.parsers

--- RestAuth/Services/cli/__init__.py
+++ RestAuth/Services/cli/__init__.py
@@ -15,54 +15,10 @@
 # You should have received a copy of the GNU General Public License
 # along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
-import fnmatch
-
-from argparse import Action, ArgumentParser
-
-from django.contrib.auth.models import Permission
-from django.contrib.contenttypes.models import ContentType
+from argparse import ArgumentParser
 
 from RestAuth.common.cli.parsers import pwd_parser
 from RestAuth.common.cli.actions import ServiceAction
-from RestAuth.Users.models import user_permissions, prop_permissions
-from RestAuth.Groups.models import group_permissions
-
-
-class PermissionParser(Action):
-    def __call__(self, parser, namespace, values, option_string):
-        user_ct = ContentType.objects.get(app_label='Users',
-                                          model='serviceuser')
-        prop_ct = ContentType.objects.get(app_label='Users', model='property')
-        group_ct = ContentType.objects.get(app_label='Groups', model='group')
-
-        user_permissions_dict = dict(user_permissions)
-        prop_permissions_dict = dict(prop_permissions)
-        group_permissions_dict = dict(group_permissions)
-
-        permissions = []
-        for value in values:
-            for codename in fnmatch.filter(user_permissions_dict.keys(), value):
-                perm, c = Permission.objects.get_or_create(
-                    content_type=user_ct, codename=codename,
-                    defaults={'name': user_permissions_dict[codename]}
-                )
-                permissions.append(perm)
-
-            for codename in fnmatch.filter(prop_permissions_dict.keys(), value):
-                perm, c = Permission.objects.get_or_create(
-                    content_type=prop_ct, codename=codename,
-                    defaults={'name': prop_permissions_dict[codename]}
-                )
-                permissions.append(perm)
-
-            for codename in fnmatch.filter(group_permissions_dict.keys(), value):
-                perm, c = Permission.objects.get_or_create(
-                    content_type=group_ct, codename=codename,
-                    defaults={'name': group_permissions_dict[codename]}
-                )
-                permissions.append(perm)
-
-        setattr(namespace, self.dest, permissions)
 
 
 # reused positional arguments:
