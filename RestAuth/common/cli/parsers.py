62a2f7f9dceb966f592bb1dc75c4e872f9526a78
Mathias Ertl <mati@fsinf.at>
2012-12-22 23:36:35 +0100

    move import to parsers, its a reserved name

--- RestAuth/common/cli/parsers.py
+++ RestAuth/common/cli/parsers.py
@@ -40,38 +40,38 @@ pwd_group.add_argument(
 ### restauth-import parser ###
 ##############################
 import_desc = "Import user data from another system."
-import_parser = ArgumentParser(description=import_desc)
+parser = ArgumentParser(description=import_desc)
 
-import_parser.add_argument(
+parser.add_argument(
     '--gen-passwords', action='store_true', default=False,
     help="Generate passwords where missing in input data and print them to "
     "stdout."
 )
-import_parser.add_argument(
+parser.add_argument(
     '--overwrite-passwords', action='store_true', default=False,
     help='Overwrite passwords of already existing services or users if the'
     'input data contains a password. (default: %(default)s)'
 )
-import_parser.add_argument(
+parser.add_argument(
     '--overwrite-properties', action='store_true', default=False,
     help='Overwrite already existing properties of users. (default: '
     '%(default)s)'
 )
-import_parser.add_argument(
+parser.add_argument(
     '--skip-existing-users', action='store_true', default=False,
     help='Skip users completely if they already exist. If not set, passwords '
     'and properties are overwritten if their respective --overwrite-... '
     'argument is given.'
 )
-import_parser.add_argument(
+parser.add_argument(
     '--skip-existing-groups', action='store_true', default=False,
     help='Skip groups completely if they already exist. If not set, users '
     'and subgroups will be added to the list.'
 )
-import_parser.add_argument(
+parser.add_argument(
     '--using', default=None, metavar="ALIAS",
     help="Use different database alias. (UNTESTED!)"
 )
 
-import_parser.add_argument(
+parser.add_argument(
     'file', nargs='?', type=argparse.FileType('r'), default=sys.stdin)
