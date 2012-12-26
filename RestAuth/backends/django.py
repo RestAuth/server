381a8910c6d913436243ecb14e288aac5eeb2dcc
Mathias Ertl <mati@fsinf.at>
2012-12-26 18:56:33 +0100

    start Django backend

---
+++ RestAuth/backends/django.py
@@ -0,0 +1,19 @@
+from RestAuth.backends.base import GroupBackend
+from RestAuth.backends.base import PropertyBackend
+from RestAuth.backends.base import UserBackend
+
+from RestAuth.common.responses import HttpResponseNoContent
+from RestAuth.Users.models import ServiceUser
+
+
+class DjangoBackend(UserBackend, PropertyBackend, GroupBackend):
+    def verify_password(self, logger, log_args, username, password):
+        user = ServiceUser.objects.only('password').get(username=username)
+
+        if not user.check_password(password):
+            # password does not match - raises 404
+            logger.info("Wrong password checked", extra=log_args)
+            raise ServiceUser.DoesNotExist("Password invalid for this user.")
+
+        logger.debug("Checked password (ok)", extra=log_args)
+        return HttpResponseNoContent()  # Ok
