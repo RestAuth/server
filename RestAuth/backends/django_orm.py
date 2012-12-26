5e9b1e23fa9239173adbf9e2acac16f6dd5455aa
Mathias Ertl <mati@fsinf.at>
2012-12-26 22:54:47 +0100

    add all message stubs

--- RestAuth/backends/django_orm.py
+++ RestAuth/backends/django_orm.py
@@ -68,7 +68,53 @@ class DjangoUserBackend(UserBackend):
 
 
 class DjangoPropertyBackend(PropertyBackend):
-    pass
+    def list(self, username):
+        # If User.DoesNotExist: 404 Not Found
+        user = User.objects.only('id').get(username=name)
+        return user.get_properties()
+
+    def create(self, username, key, value):
+        raise NotImplementedError
+
+    def get(self, username, key):
+        raise NotImplementedError
+
+    def set(self, username, key, value):
+        raise NotImplementedError
+
+    def set_multiple(self, username, props):
+        raise NotImplementedError
+
+    def remove(self, username, key):
+        raise NotImplementedError
 
 class DjangoGroupBackend(GroupBackend):
-    pass
+    def create(self, service, groupname):
+        raise NotImplementedError
+
+    def exists(self, service, groupname):
+        raise NotImplementedError
+
+    def add_user(self, service, groupname, username):
+        raise NotImplementedError
+
+    def users(self, service, groupname):
+        raise NotImplementedError
+
+    def member(self, service, groupname, username):
+        raise NotImplementedError
+
+    def rm_user(self, service, groupname, username):
+        raise NotImplementedError
+
+    def add_subgroup(self, service, groupname, subservice, subgroupname):
+        raise NotImplementedError
+
+    def subgroups(self, service, groupname, subservice):
+        raise NotImplementedError
+
+    def rm_subgroup(self, service, groupname, subservice, subgroupname):
+        raise NotImplementedError
+
+    def remove(self, service, groupname):
+        raise NotImplementedError
