58a00dcd0d96b4a3e05a03ea5f7baa7d77e6c338
Mathias Ertl <mati@fsinf.at>
2012-12-26 20:22:14 +0100

    alias ServiceUser to "User" for shorter code

--- RestAuth/backends/django.py
+++ RestAuth/backends/django.py
@@ -2,16 +2,16 @@ from RestAuth.backends.base import GroupBackend
 from RestAuth.backends.base import PropertyBackend
 from RestAuth.backends.base import UserBackend
 
-from RestAuth.Users.models import ServiceUser
+from RestAuth.Users.models import ServiceUser as User
 
 
 class DjangoUserBackend(UserBackend):
     def list(self):
-        return list(ServiceUser.objects.values_list('username', flat=True))
+        return list(User.objects.values_list('username', flat=True))
 
     def verify_password(self, username, password):
         # If User.DoesNotExist: 404 Not Found
-        user = ServiceUser.objects.only('password').get(username=username)
+        user = User.objects.only('password').get(username=username)
 
         return user.check_password(password)
 
