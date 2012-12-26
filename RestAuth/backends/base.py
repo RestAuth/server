39e7704f20459dfc816fe97c1a2d9197ec94be90
Mathias Ertl <mati@fsinf.at>
2012-12-26 19:40:27 +0100

    add stubs for all methods

--- RestAuth/backends/base.py
+++ RestAuth/backends/base.py
@@ -1,9 +1,73 @@
 class UserBackend(object):
-    def check_password(self, logger, log_args, username, password):
+    def list(self, username, password, log, log_args):
         raise NotImplementedError
 
+    def create(self, username, password, properties, log):
+        raise NotImplementedError
+
+    def exists(self, username, log, log_args):
+        raise NotImplementedError
+
+    def check_password(self, username, password, log, log_args):
+        raise NotImplementedError
+
+    def set_password(self, username, password, log, log_args):
+        raise NotImplementedError
+
+    def remove(self, username, log, log_args):
+        raise NotImplementedError
+
+
 class PropertyBackend(object):
-    pass
+    def list(self, username, log, log_args):
+        raise NotImplementedError
+
+    def create(self, username, key, value, log, log_args):
+        raise NotImplementedError
+
+    def get(self, username, key, log, log_args):
+        raise NotImplementedError
+
+    def set(self, username, key, value, log, log_args):
+        raise NotImplementedError
+
+    def set_multiple(self, username, props, log, log_args):
+        raise NotImplementedError
+
+    def remove(self, username, key, log, log_args):
+        raise NotImplementedError
+
 
 class GroupBackend(object):
-    pass
+    def create(self, service, groupname, log, log_args):
+        raise NotImplementedError
+
+    def exists(self, service, groupname, log, log_args):
+        raise NotImplementedError
+
+    def add_user(self, service, groupname, username, log, log_args):
+        raise NotImplementedError
+
+    def users(self, service, groupname, log, log_args):
+        raise NotImplementedError
+
+    def member(self, service, groupname, username):
+        raise NotImplementedError
+
+    def rm_user(self, service, groupname, username):
+        raise NotImplementedError
+
+    def add_subgroup(self, service, groupname, subservice, subgroupname,
+                  log, log_args):
+        raise NotImplementedError
+
+    def subgroups(self, service, groupname, subservice, log, log_args):
+        raise NotImplementedError
+
+    def rm_subgroup(self, service, groupname, subservice, subgroupname,
+                 log, log_args):
+        raise NotImplementedError
+
+    def remove(self, service, groupname, log, log_args):
+        raise NotImplementedError
+
