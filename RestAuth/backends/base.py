a2525343b619ee49d19f404e14627735d2e83040
Mathias Ertl <mati@fsinf.at>
2012-12-31 12:20:24 +0100

    accept dry parameter for PropertyBackend.set_multiple

--- RestAuth/backends/base.py
+++ RestAuth/backends/base.py
@@ -15,7 +15,24 @@
 # You should have received a copy of the GNU General Public License
 # along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
 
+from django.conf import settings
+
+from RestAuth.common.utils import import_path
+
+
 class UserBackend(object):
+    property_backend = None
+
+    @classmethod
+    def _get_property_backend(cls):
+        if cls.property_backend is None:
+            cls.property_backend = import_path(getattr(
+                settings, 'PROPERTY_BACKEND',
+                'RestAuth.backends.django_orm.DjangoPropertyBackend'
+            ))[0]()
+
+        return cls.property_backend
+
     def get(self, username):
         raise NotImplementedError
 
@@ -51,7 +68,7 @@ class PropertyBackend(object):
     def set(self, username, key, value):
         raise NotImplementedError
 
-    def set_multiple(self, username, props):
+    def set_multiple(self, username, props, dry=False):
         raise NotImplementedError
 
     def remove(self, username, key):
