7f393a32e10664b9431b687902442cc5ca0bcf2e
Mathias Ertl <mati@fsinf.at>
2012-12-31 17:54:14 +0100

    remove last django orm code from Test unittests

--- RestAuth/Test/tests.py
+++ RestAuth/Test/tests.py
@@ -7,9 +7,8 @@ Replace this with more appropriate tests for your application.
 
 import httplib
 
-from RestAuth.Groups.models import Group
-from RestAuth.Users.models import ServiceUser
 from RestAuth.common.testdata import groupname1
+from RestAuth.common.testdata import group_backend
 from RestAuth.common.testdata import password1
 from RestAuth.common.testdata import propkey1
 from RestAuth.common.testdata import propval1
@@ -24,49 +23,48 @@ class CreateUserTest(RestAuthTransactionTest):
     def test_dry_run_create_user(self):
         resp = self.post('/test/users/', {'user': username1})
         self.assertEqual(resp.status_code, httplib.CREATED)
-        self.assertFalse(ServiceUser.objects.all())
+        self.assertFalse(user_backend.list())
 
     def test_dry_run_create_user_with_pass(self):
         resp = self.post(
             '/test/users/', {'user': username1, 'password': password1})
         self.assertEqual(resp.status_code, httplib.CREATED)
-        self.assertFalse(ServiceUser.objects.all())
+        self.assertFalse(user_backend.list())
 
     def test_dry_run_create_user_with_props(self):
         resp = self.post('/test/users/', {'user': username1,
                                           'properties': {'foo': 'bar'}})
         self.assertEqual(resp.status_code, httplib.CREATED)
-        self.assertFalse(ServiceUser.objects.all())
+        self.assertFalse(user_backend.list())
 
     def test_dry_run_create_user_with_pass_and_props(self):
         content = {'user': username1, 'password': password1,
                    'properties': {'foo': 'bar'}}
         resp = self.post('/test/users/', content)
         self.assertEqual(resp.status_code, httplib.CREATED)
-        self.assertFalse(ServiceUser.objects.all())
+        self.assertFalse(user_backend.list())
 
     def test_dry_run_create_existing_user(self):
-        user = ServiceUser.objects.create(username=username1)
-        user.save()
+        user = user_backend.create(username=username1)
 
         resp = self.post('/test/users/', {'user': username1})
         self.assertEqual(resp.status_code, httplib.CONFLICT)
-        self.assertItemsEqual([user], ServiceUser.objects.all())
+        self.assertItemsEqual([user.username], user_backend.list())
 
     def test_dry_run_create_invalid_user(self):
         resp = self.post('/test/users/', {'user': 'foo/bar'})
         self.assertEqual(resp.status_code, httplib.PRECONDITION_FAILED)
-        self.assertFalse(ServiceUser.objects.all())
+        self.assertFalse(user_backend.list())
 
     def test_dry_run_create_short_user(self):
         resp = self.post('/test/users/', {'user': 'x'})
         self.assertEqual(resp.status_code, httplib.PRECONDITION_FAILED)
-        self.assertFalse(ServiceUser.objects.all())
+        self.assertFalse(user_backend.list())
 
     def test_create_with_too_short_pass(self):
         resp = self.post('/test/users/', {'user': username1, 'password': 'a'})
         self.assertEqual(resp.status_code, httplib.PRECONDITION_FAILED)
-        self.assertFalse(ServiceUser.objects.all())
+        self.assertFalse(user_backend.list())
 
 
 class CreatePropertyTest(RestAuthTransactionTest):
@@ -119,17 +117,16 @@ class CreateGroupTest(RestAuthTransactionTest):
     def test_dry_run_create_group(self):
         resp = self.post('/test/groups/', {'group': groupname1})
         self.assertEqual(resp.status_code, httplib.CREATED)
-        self.assertFalse(Group.objects.all())
+        self.assertFalse(group_backend.list(self.service))
 
     def test_dry_run_create_existing_group(self):
-        group = Group(name=groupname1, service=self.service)
-        group.save()
+        group = group_backend.create(service=self.service, name=groupname1)
 
         resp = self.post('/test/groups/', {'group': groupname1})
         self.assertEqual(resp.status_code, httplib.CONFLICT)
-        self.assertItemsEqual([group], Group.objects.all())
+        self.assertItemsEqual([group.name], group_backend.list(self.service))
 
     def test_dry_run_create_invalid_group(self):
         resp = self.post('/test/groups/', {'group': 'foo/bar'})
         self.assertEqual(resp.status_code, httplib.PRECONDITION_FAILED)
-        self.assertFalse(Group.objects.all())
+        self.assertFalse(group_backend.list(self.service))
