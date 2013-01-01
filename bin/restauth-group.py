4c5c9d8c7f5607499f940e2513253576f55a84ea
Mathias Ertl <mati@fsinf.at>
2013-01-01 11:55:35 +0100

    use django backend

--- bin/restauth-group.py
+++ bin/restauth-group.py
@@ -23,15 +23,22 @@ if 'DJANGO_SETTINGS_MODULE' not in os.environ:
     os.environ['DJANGO_SETTINGS_MODULE'] = 'RestAuth.settings'
 sys.path.append(os.getcwd())
 
-from django.db.utils import IntegrityError
-
 try:
-    from RestAuth.Groups.models import Group
+    from django.conf import settings
+
     from RestAuth.Groups.cli.parsers import parser, get_group, print_by_service
+    from RestAuth.common.errors import GroupExists, GroupNotFound
+    from RestAuth.common.errors import UserNotFound
+    from RestAuth.common.utils import import_path
 except ImportError as e:
     sys.stderr.write('Error: Cannot import RestAuth. '
                      'Please make sure RestAuth is in your PYTHONPATH.\n')
     sys.exit(1)
+
+group_backend = import_path(getattr(
+        settings, 'GROUP_BACKEND',
+        'RestAuth.backends.django_orm.DjangoGroupBackend'
+))[0]()
 
 # parse arguments
 args = parser.parse_args()
@@ -39,30 +46,30 @@ args = parser.parse_args()
 # Actions that do not act on an existing group:
 if args.action == 'add':
     try:
-        Group.objects.create(name=args.group, service=args.service)
-    except IntegrityError:
+        group_backend.create(name=args.group, service=args.service)
+    except GroupExists:
         parser.error('Group already exists.')
 elif args.action in ['list', 'ls']:
-    qs = Group.objects.values_list('name', flat=True).order_by('name')
     if args.service:
-        groups = qs.filter(service=args.service)
+        groups = group_backend.list(service=args.service)
     else:
-        groups = qs.filter(service=None)
-    for group in groups:
-        print(group.encode('utf-8'))
+        groups = group_backend.list(service=None)
+    for name in sorted(groups):
+        print(name.encode('utf-8'))
 elif args.action == 'view':
     group = get_group(parser, args.group, args.service)
-    explicit_users = group.get_members(depth=0).values_list(
-        'username', flat=True)
-    effective_users = group.get_members().values_list('username', flat=True)
-    parent_groups = list(group.parent_groups.all())
-    sub_groups = list(group.groups.all())
+
+    explicit_users = sorted(group_backend.members(group, depth=0))
+    effective_users = sorted(group_backend.members(group))
+    parent_groups = sorted(group_backend.parents(group))
+    sub_groups = sorted(group_backend.subgroups(group, filter=False))
+
     if explicit_users:
-        print('* Explicit members: %s' % ', '.join(sorted(explicit_users)))
+        print('* Explicit members: %s' % ', '.join(explicit_users))
     else:
         print('* No explicit members')
     if effective_users:
-        print('* Effective members: %s' % ', '.join(sorted(effective_users)))
+        print('* Effective members: %s' % ', '.join(effective_users))
     else:
         print('* No effective members')
     if parent_groups:
@@ -77,22 +84,27 @@ elif args.action == 'view':
         print('* No subgroups')
 elif args.action == 'add-user':
     group = get_group(parser, args.group, args.service)
-    group.users.add(args.user)
+    group_backend.add_user(group, args.user)
 elif args.action == 'add-group':
     group = get_group(parser, args.group, args.service)
-    sub_group = get_group(parser, args.subgroup, args.sub_service)
+    subgroup = get_group(parser, args.subgroup, args.sub_service)
 
-    sub_group.parent_groups.add(group)
+    group_backend.add_subgroup(group, subgroup)
 elif args.action in ['delete', 'del', 'rm']:
-    group = get_group(parser, args.group, args.service)
-    group.delete()
+    group_backend.remove(args.service, args.group)
 elif args.action in ['remove-user', 'rm-user', 'del-user']:
     group = get_group(parser, args.group, args.service)
-    if args.user in group.users.all():
-        group.users.remove(args.user)
+    try:
+        group_backend.rm_user(group, args.user)
+    except UserNotFound:
+        parser.error('User "%s" not member of group %s.' %
+                     (args.user.username, group.name))
 elif args.action in ['remove-group', 'rm-group', 'del-group']:
     group = get_group(parser, args.group, args.service)
-    sub_group = get_group(parser, args.subgroup, args.sub_service)
+    subgroup = get_group(parser, args.subgroup, args.sub_service)
 
-    if sub_group in group.groups.all():
-        sub_group.parent_groups.remove(group)
+    try:
+        group_backend.rm_subgroup(group, subgroup)
+    except GroupNotFound:
+        parser.error('Group "%s" is not a subgroup of "%s".' %
+                     (subgroup.name, group.name))
