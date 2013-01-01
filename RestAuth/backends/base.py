e6a940cc044a3efabbb6a358aa2f141882fb79aa
Mathias Ertl <mati@fsinf.at>
2013-01-01 15:41:26 +0100

    add loads of documentation (not yet finished

--- RestAuth/backends/base.py
+++ RestAuth/backends/base.py
@@ -17,47 +17,185 @@
 
 
 class UserInstance(object):
-    def __init__(self, id, name):
+    """Class representing a user.
+
+    Instances of this class should provide the ``username`` and ``id``
+    property.
+
+    * The ``username`` is the username of the user as used by the protocol.
+    * The ``id`` may be the same as the username or some backend
+      specific id, i.e. one that allows faster access.
+    """
+
+    def __init__(self, id, username):
         self.id = id
-        self.name = name
+        self.username = username
 
 
 class GroupInstance(object):
-    def __init__(self, name):
+    """Class representing a group.
+
+    Instances of this class should provide the ``name``, ``id`` and ``service``
+    properties. For the ``name`` and ``id`` properties, the same semantics as
+    for :py:class:`~RestAuth.backends.base.UserInstance` apply.
+
+    The ``service`` property is a service as configured by
+    |bin-restauth-service|. Its name is (confusingly!) available as its
+    ``username`` property.
+    """
+
+    def __init__(self, id, name, service):
+        self.id = id
         self.name = name
+        self.service = service
 
 
 class UserBackend(object):
+    """Provide the most basic user operations and password management."""
+
     def get(self, username):
+        """Get a user object for the given username.
+
+        This method is used to get user objects passed to property- and
+        group-backends.
+
+        :param username: The username.
+        :type  username: str
+        :return: A user object providing at least the properties of the
+            UserInstance class.
+        :rtype: :py:class:`~.UserInstance`
+        :raise: :py:class:`~RestAuth.common.errors.UserNotFound` if the user
+            doesn't exist.
+        """
         raise NotImplementedError
 
     def list(self):
+        """Get a list of all usernames.
+
+        Each element of the returned list should be a valid username that can
+        be passed to :py:meth:`~.UserBackend.get`.
+
+        :return: A list of usernames.
+        :rtype: list
+        """
         raise NotImplementedError
 
     def create(self, username, password=None, properties=None,
                property_backend=None, dry=False):
+        """Create a new user.
+
+        The ``username`` is already validated, so you don't need to do any
+        additional validation here. If your backend has username restrictions,
+        please implement a :ref:`username validator <implement-validators>`.
+
+        If ``properties`` are passed, please use the property backend passed to
+        store the properties:
+
+        .. code-block:: python
+
+           user = ...  # create the user
+           property_backend.set_multiple(user, properties, dry=dry)
+           return user
+
+        The ``dry`` parameter tells you if you should actually create the user.
+        The parameter will be True for `dry-runs
+        <https://restauth.net/wiki/Specification#Doing_dry-runs>`_. In a
+        dry-run, the method should behave as closely as possible to a normal
+        invocation but shouldn't actually create the user.
+
+        :param username: The username.
+        :type  username: str
+        :param password: The password to set. If not given, the user should
+            not have a valid password and is unable to log in.
+        :type  password: str
+        :param properties: Any initial properties for the user.
+        :type  properties: dict
+        :param property_backend: The backend to use to store properties.
+        :type  property_backend: :py:class:`~.PropertyBackend`
+        :param dry: Wether or not to actually create the user.
+        :type  dry: boolean
+        :return: A user object providing at least the properties of the
+            UserInstance class.
+        :rtype: :py:class:`~.UserInstance`
+        :raise: :py:class:`~RestAuth.common.errors.UserExists` if the user
+            already exist.
+        """
         raise NotImplementedError
 
     def exists(self, username):
+        """Determine if the username exists.
+
+        :param username: The username.
+        :type  username: str
+        :return: True if the user exists, False otherwise.
+        :rtype: boolean
+        """
         raise NotImplementedError
 
     def check_password(self, username, password):
+        """Check a users password.
+
+        :param username: The username.
+        :type  username: str
+        :param password: The password to check.
+        :type  password: str
+        :return: True if the password exists, False otherwise.
+        :rtype: boolean
+        :raise: :py:class:`~RestAuth.common.errors.UserNotFound` if the user
+            doesn't exist.
+        """
         raise NotImplementedError
 
-    def set_password(self, username, password):
+    def set_password(self, username, password=None):
+        """Set a new password.
+
+        :param username: The username.
+        :type  username: str
+        :param password: The new password. If None or empty, the user should
+            get an unusable password.
+        :type  password: str
+        :raise: :py:class:`~RestAuth.common.errors.UserNotFound` if the user
+            doesn't exist.
+        """
         raise NotImplementedError
 
     def remove(self, username):
+        """Remove a user.
+
+        :param username: The username.
+        :type  username: str
+        :raise: :py:class:`~RestAuth.common.errors.UserNotFound` if the user
+            doesn't exist.
+        """
         raise NotImplementedError
 
     def testSetUp(self):
+        """Set up your backend for a test run.
+
+        This method is exclusively used in unit tests. It should perform any
+        actions necessary to start a unit test.
+
+        .. NOTE:: You do not need to implement this method, if there is nothing
+           to do.
+        """
         pass
 
     def testTearDown(self):
+        """Tear down your backend after a test run.
+
+        This method is exclusively used in unit tests. It should perform any
+        actions necessary after a unit test. In general, this should completely
+        wipe all users created during a unit test.
+
+        .. NOTE:: You do not need to implement this method if the backend
+           automatically cleans itself.
+        """
         pass
 
 
 class PropertyBackend(object):
+    """todo."""
+
     def list(self, user):
         raise NotImplementedError
 
@@ -77,13 +215,32 @@ class PropertyBackend(object):
         raise NotImplementedError
 
     def testSetUp(self):
+        """Set up your backend for a test run.
+
+        This method is exclusively used in unit tests. It should perform any
+        actions necessary to start a unit test.
+
+        .. NOTE:: You do not need to implement this method, if there is nothing
+           to do.
+        """
         pass
 
     def testTearDown(self):
+        """Tear down your backend after a test run.
+
+        This method is exclusively used in unit tests. It should perform any
+        actions necessary after a unit test. In general, this should completely
+        wipe all users and properties created during a unit test.
+
+        .. NOTE:: You do not need to implement this method if the backend
+           automatically cleans itself.
+        """
         pass
 
 
 class GroupBackend(object):
+    """todo."""
+
     def get(self, service, name):
         raise NotImplementedError
 
@@ -124,7 +281,24 @@ class GroupBackend(object):
         raise NotImplementedError
 
     def testSetUp(self):
+        """Set up your backend for a test run.
+
+        This method is exclusively used in unit tests. It should perform any
+        actions necessary to start a unit test.
+
+        .. NOTE:: You do not need to implement this method, if there is nothing
+           to do.
+        """
         pass
 
     def testTearDown(self):
+        """Tear down your backend after a test run.
+
+        This method is exclusively used in unit tests. It should perform any
+        actions necessary after a unit test. In general, this should completely
+        wipe all users and groups created during a unit test.
+
+        .. NOTE:: You do not need to implement this method if the backend
+           automatically cleans itself.
+        """
         pass
