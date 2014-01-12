910ea804593c1865bfae3d8b6926b877a5d30b3f
Mathias Ertl <mati@fsinf.at>
2014-01-12 15:34:48 +0100

    tw=99

--- RestAuth/common/content_handlers.py
+++ RestAuth/common/content_handlers.py
@@ -2,18 +2,16 @@
 #
 # This file is part of RestAuth (https://restauth.net).
 #
-# RestAuth is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# RestAuth is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# RestAuth is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# RestAuth is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with RestAuth.  If not,
+# see <http://www.gnu.org/licenses/>.
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
@@ -40,8 +38,7 @@ def load_handlers():
 
         handler = handler_cls()
         if not getattr(handler, 'mime'):
-            raise ImproperlyConfigured("Handler doesn't specify a "
-                                       "MIME type: %s" % backend)
+            raise ImproperlyConfigured("Handler doesn't specify a MIME type: %s" % backend)
         handlers.append(handler)
 
     HANDLERS = dict([(h.mime, h) for h in handlers])
@@ -59,8 +56,9 @@ def get_handler(mimetype=None):
         try:
             return HANDLERS[mimetype]
         except KeyError:
-            raise ValueError("Unknown mimetype %s. Did you specify it in the "
-                             "CONTENT_HANDLERS setting?" % mimetype)
+            raise ValueError(
+                "Unknown mimetype %s. Specify a handler in the CONTENT_HANDLERS setting." %
+                mimetype)
 
 
 def get_supported():
