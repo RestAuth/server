d20804fa6e9cc2be7b98d3e2c2511fa0b6b4cd49
Mathias Ertl <mati@fsinf.at>
2014-01-12 14:57:41 +0100

    code beautification

--- RestAuth/Users/validators.py
+++ RestAuth/Users/validators.py
@@ -2,18 +2,16 @@
 #
 # This file is part of RestAuth (https://restauth.net).
 #
-# RestAuth is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# RestAuth is free software: you can redistribute it and/or modify it under the terms of the GNU
+# General Public License as published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
 #
-# RestAuth is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# RestAuth is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with RestAuth.  If not,
+# see <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals  # unicode literals from python
 
@@ -88,8 +86,7 @@ def validate_username(username):
     # check for illegal characters:
     for char in USERNAME_ILLEGAL_CHARS:
         if char in username:
-            raise UsernameInvalid(
-                "Username must not contain character '%s'" % char)
+            raise UsernameInvalid("Username must not contain character '%s'" % char)
 
     # reserved names
     if username in USERNAME_RESERVED:
@@ -103,8 +100,7 @@ def validate_username(username):
             else:  # pragma: py2
                 username.decode('ascii')
         except (UnicodeDecodeError, UnicodeEncodeError):
-            raise UsernameInvalid(
-                "Username must only contain ASCII characters")
+            raise UsernameInvalid("Username must only contain ASCII characters")
 
     # check for whitespace
     if USERNAME_NO_WHITESPACE is not False:
@@ -139,10 +135,9 @@ class Validator(object):
 
 
 class XMPPValidator(Validator):
-    """
-    This validator ensures that usernames are valid username-parts for
-    Jabber/XMPP accounts. You can use this validator if you want to provide
-    XMPP-addresses of the form ``<username>@example.com``.
+    """This validator ensures that usernames are valid username-parts for Jabber/XMPP accounts. You
+    can use this validator if you want to provide XMPP-addresses of the form
+    ``<username>@example.com``.
 
     This validator applies the following restrictions:
 
@@ -155,10 +150,9 @@ class XMPPValidator(Validator):
 
 
 class EmailValidator(Validator):
-    """
-    This validator ensures that usernames are valid username-parts of
-    email-addresses. You can use this validator if you want to provide
-    email-addresses of the form ``<username>@example.com``.
+    """This validator ensures that usernames are valid username-parts of email-addresses. You can
+    use this validator if you want to provide email-addresses of the form
+    ``<username>@example.com``.
 
     This validator applies the following restrictions:
 
@@ -177,13 +171,11 @@ class EmailValidator(Validator):
 
     def check(self, name):
         if len(name) > 64:
-            raise UsernameInvalid(
-                "Username must be no longer than 64 characters.")
+            raise UsernameInvalid("Username must be no longer than 64 characters.")
 
 
 class MediaWikiValidator(Validator):
-    """
-    This validator ensures that usernames are compatible with
+    """This validator ensures that usernames are compatible with
     `MediaWiki <http://www.mediawiki.org>`_.
 
     This validator applies the following restrictions:
@@ -210,13 +202,11 @@ class MediaWikiValidator(Validator):
     def check(self, name):
         if len(name.encode('utf-8')) > 255:  # pragma: no cover
             # Page titles only up to 255 bytes:
-            raise UsernameInvalid(
-                "Username must not be longer than 255 characters")
+            raise UsernameInvalid("Username must not be longer than 255 characters")
 
 
 class LinuxValidator(Validator):
-    """
-    This validator ensures that usernames are Linux system users.
+    """This validator ensures that usernames are Linux system users.
 
     This validator applies the following restrictions:
 
@@ -237,46 +227,38 @@ class LinuxValidator(Validator):
 
     def check(cls, name):
         if name.startswith('-'):
-            raise UsernameInvalid(
-                '%s: Username must not start with a dash ("-")' % name)
+            raise UsernameInvalid('%s: Username must not start with a dash ("-")' % name)
         if len(name) > 32:
-            raise UsernameInvalid(
-                '%s: Username must not be longer than 32 characters' % name)
-        if not settings.RELAXED_LINUX_CHECKS and \
-                not re.match('[a-z_ ][a-z0-9_-]*[$]?', name):
-            raise UsernameInvalid(
-                '%s: Username must match regex "[a-z_][a-z0-9_-]*[$]?"' % name)
+            raise UsernameInvalid('%s: Username must not be longer than 32 characters' % name)
+        if not settings.RELAXED_LINUX_CHECKS and not re.match('[a-z_ ][a-z0-9_-]*[$]?', name):
+            raise UsernameInvalid('%s: Username must match regex "[a-z_][a-z0-9_-]*[$]?"' % name)
 
 
 class WindowsValidator(Validator):
-    """
-    This validator ensures that usernames are valid Windows system users.
+    """This validator ensures that usernames are valid Windows system users.
 
-    .. WARNING:: This validator is completely untested as no Windows systems
-       are available. This means that this validator is likely not allow some
-       accounts that are in fact not valid. If you can, please consider
-       to :ref:`contributing <contribute-validators>`.
+    .. WARNING:: This validator is completely untested as no Windows systems are available. This
+       means that this validator is likely not allow some accounts that are in fact not valid. If
+       you can, please consider to :ref:`contributing <contribute-validators>`.
 
     This validator applies the following restrictions:
 
     * All characters must be ASCII characters
     * The following characters are forbidden: ``"``, ``[``, ``]``, ``;``,
       ``|``, ``=``, ``+``, ``*``, ``?``, ``<`` and ``>``
-    * The following usernames are reserved and thus blocked:
-      ``anonymous``, ``authenticated user``, ``batch``, ``builtin``,
-      ``creator group``, ``creator group server``, ``creator owner``,
-      ``creator owner server``, ``dialup``, ``digest auth``, ``interactive``,
-      ``internet``, ``local``, ``local system``, ``network``,
-      ``network service``, ``nt authority``, ``nt domain``, ``ntlm auth``,
-      ``null``, ``proxy``, ``remote interactive``, ``restricted``,
-      ``schannel auth``, ``self``, ``server``, ``service``, ``system``,
-      ``terminal server``, ``this organization``, ``users`` and ``world``
+    * The following usernames are reserved and thus blocked: ``anonymous``, ``authenticated user``,
+      ``batch``, ``builtin``, ``creator group``, ``creator group server``, ``creator owner``,
+      ``creator owner server``, ``dialup``, ``digest auth``, ``interactive``, ``internet``,
+      ``local``, ``local system``, ``network``, ``network service``, ``nt authority``,
+      ``nt domain``, ``ntlm auth``, ``null``, ``proxy``, ``remote interactive``, ``restricted``,
+      ``schannel auth``, ``self``, ``server``, ``service``, ``system``, ``terminal server``,
+      ``this organization``, ``users`` and ``world``
 
     For a list of reserved windows usernames, see `this KnowledgeBase article
     <http://support.microsoft.com/kb/909264>`_.
     """
     ILLEGAL_CHARACTERS = set([
-        '"', '[', ']', ';', '|', '=', '+', '*', '?', '<', '>'
+        '"', '[', ']', ';', '|', '=', '+', '*', '?', '<', '>',
     ])
     FORCE_ASCII = True
     RESERVED = set([
@@ -291,9 +273,7 @@ class WindowsValidator(Validator):
 
 
 class DrupalValidator(Validator):
-    """
-    This validator ensures that usernames are valid
-    `Drupal <https://drupal.or>`_ usernames.
+    """This validator ensures that usernames are valid `Drupal <https://drupal.or>`_ usernames.
 
     This validator applies the following restrictions:
 
