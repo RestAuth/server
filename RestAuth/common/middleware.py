2bef81d80c9211e13f0536272373764bec2d0eff
Mathias Ertl <mati@er.tl>
2015-01-23 13:07:50 +0100

    set a version property to request

--- RestAuth/common/middleware.py
+++ RestAuth/common/middleware.py
@@ -39,6 +39,8 @@ CONTENT_TYPE_METHODS = set(['POST', 'PUT'])
 class RestAuthMiddleware:
     def process_request(self, request):
         """Middleware to ensure required headers are present."""
+        version = request.META.get('X_RESTAUTH_VERSION', '0.7')
+        request.version = tuple(version.split('.'))
 
         if request.method in CONTENT_TYPE_METHODS:
             if 'CONTENT_TYPE' not in request.META:
