a67db412fad2c052a8f7eb94f04c73f7035486b7
Mathias Ertl <mati@er.tl>
2017-07-24 16:07:11 +0200

    update urls.py

--- RestAuth/Groups/urls.py
+++ RestAuth/Groups/urls.py
@@ -2,20 +2,19 @@
 #
 # This file is part of RestAuth (https://restauth.net).
 #
-# RestAuth is free software: you can redistribute it and/or modify it under the terms of the GNU
-# General Public License as published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# RestAuth is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
-# RestAuth is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# RestAuth is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
 #
-# You should have received a copy of the GNU General Public License along with RestAuth. If not,
-# see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with RestAuth. If not, see
+# <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns
 from django.conf.urls import url
 
 from Groups.views import GroupGroupHandler
@@ -26,9 +25,7 @@ from Groups.views import GroupUsersIndex
 from Groups.views import GroupsView
 from Services.decorator import login_required
 
-urlpatterns = patterns(
-    'Groups.views',
-
+urlpatterns = [
     url(r'^$', login_required(realm='/groups/')(GroupsView.as_view()), name='groups'),
     url(r'^(?P<name>[^/]+)/$',
         login_required(realm='/groups/<group>/')(GroupHandlerView.as_view()), name='groups.group'),
@@ -45,4 +42,4 @@ urlpatterns = patterns(
         login_required(realm='/groups/<group>/groups/<subgroupname>')(
             GroupGroupHandler.as_view()),
         name='groups.group.groups.group'),
-)
+]
--- RestAuth/Test/urls.py
+++ RestAuth/Test/urls.py
@@ -2,25 +2,26 @@
 #
 # This file is part of RestAuth (https://restauth.net).
 #
-# RestAuth is free software: you can redistribute it and/or modify it under the terms of the GNU
-# General Public License as published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# RestAuth is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
-# RestAuth is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
-# even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-# General Public License for more details.
+# RestAuth is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
 #
-# You should have received a copy of the GNU General Public License along with RestAuth. If not,
-# see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with RestAuth. If not, see
+# <http://www.gnu.org/licenses/>.
 
 from __future__ import unicode_literals
 
-from django.conf.urls import patterns
 from django.conf.urls import url
 
-urlpatterns = patterns(
-    'Test.views',
-    url(r'^users/$', 'users'),
-    url(r'^users/(?P<name>[^/]+)/props/$', 'users_user_props'),
-    url(r'^groups/$', 'groups'),
-)
+from . import views
+
+
+urlpatterns = [
+    url(r'^users/$', views.users),
+    url(r'^users/(?P<name>[^/]+)/props/$', views.users_user_props),
+    url(r'^groups/$', views.groups),
+]
