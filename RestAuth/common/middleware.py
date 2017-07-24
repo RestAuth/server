1171b6e410117ca108d039606c4df2f65de47cbd
Mathias Ertl <mati@er.tl>
2017-07-24 16:48:57 +0200

    use middlewaare mixin

--- RestAuth/common/middleware.py
+++ RestAuth/common/middleware.py
@@ -2,18 +2,16 @@
 #
 # This file is part of RestAuth (https://restauth.net).
 #
-# RestAuth is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# RestAuth is free software: you can redistribute it and/or modify it under the terms of the GNU General
+# Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
 #
-# RestAuth is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# RestAuth is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with RestAuth.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with RestAuth. If not, see
+# <http://www.gnu.org/licenses/>.
 
 """
 The ExceptionMiddleware is located in its own class to avoid circular imports.
@@ -25,6 +23,7 @@ import logging
 import traceback
 
 from django.http import HttpResponse
+from django.utils.deprecation import MiddlewareMixin
 
 from RestAuthCommon.error import RestAuthException
 
@@ -36,7 +35,7 @@ log = logging.getLogger(__name__)
 CONTENT_TYPE_METHODS = {'POST', 'PUT'}
 
 
-class RestAuthMiddleware:
+class RestAuthMiddleware(MiddlewareMixin, object):
     def process_request(self, request):
         """Middleware to ensure required headers are present."""
         version = request.META.get('X_RESTAUTH_VERSION', '0.6')
